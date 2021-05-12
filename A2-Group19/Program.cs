7f74257e4295f2bfbcf2f76e2623900cde4e0acc
Tuan Dung Nguyen <dung.tuannguyen248@gmail.com>
2021-05-13 05:07:22 +1000

    Add project files.

---
+++ A2-Group19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A2_Group19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
